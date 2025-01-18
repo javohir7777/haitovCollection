8998dd6fa88cb04b1e6d910ad54386df0fb5a817
javohir7777 <javohirjumayev7777@gmail.com>
2025-01-18 12:47:09 +0500

    add to responsiv add and receipts

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,6 +2,8 @@ import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../navbar";
 import Header from "../header";
+import Nav2 from "../navbar/nav-2";
+
 import "./layout.css";
 const Layout = () => {
   return (
@@ -9,6 +11,9 @@ const Layout = () => {
       <nav>
         <Navbar />
       </nav>
+      <div className="nav-2">
+        <Nav2 />
+      </div>
       <section className="dashboard">
         <Header />
         <Outlet />
